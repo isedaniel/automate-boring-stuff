3d7d16a176a72c2c2a38018edd240a7af186c77b
Daniel <ise.daniel@gmail.com>
2023-11-16 17:38:36 -0300

    add added

--- src/chapter5/inventory.py
+++ src/chapter5/inventory.py
@@ -1,6 +1,6 @@
 class Inventory:
-    default_inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-    def __init__(self, inventory=default_inventory):
+    default = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+    def __init__(self, inventory=default):
         self.inventory = inventory
 
     def display(self):
@@ -12,5 +12,14 @@ class Inventory:
         s += f"Total: {total}"
         return s
 
+    def add(self, items):
+        for i in items:
+            self.inventory.setdefault(i, 0)
+            self.inventory[i] += 1
+
+
 i = Inventory()
+print(i.display())
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+i.add(dragon_loot)
 print(i.display())
